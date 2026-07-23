18dcd27d66fcea558a0931a7ab6a5b2d1868bd33
Tristan <tristanacosta03@gmail.com>
2026-07-23 21:35:36 +0800

    Add weather icons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./styles/main.css";
+import Sunny from "./assets/icons/sunny.svg";
+
+const bg = document.querySelector("#bg");
+
+fetch(Sunny)
+    .then((response) => response.text())
+    .then((svg) => {
+        bg.innerHTML = svg;
+    });
