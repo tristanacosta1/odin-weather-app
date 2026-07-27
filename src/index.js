042fdbb173b51a99f16b58d8e3dfdb6ad89c8c24
Tristan <tristanacosta03@gmail.com>
2026-07-27 11:30:58 +0800

    Refactor layout to separate icon from main flow

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import "./styles/main.css";
 import Sunny from "./assets/icons/sunny.svg";
 
-const bg = document.querySelector("#bg");
+const icon = document.querySelector("#icon");
 
 fetch(Sunny)
     .then((response) => response.text())
     .then((svg) => {
-        bg.innerHTML = svg;
+        icon.innerHTML = svg;
     });
