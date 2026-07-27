1506350dbfb1424699d285ad90a418f004f008a3
Tristan <tristanacosta03@gmail.com>
2026-07-27 21:44:06 +0800

    Add dependency for globals and start main layout

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
 
 export default [
     {
@@ -17,9 +18,7 @@ export default [
             sourceType: "module",
 
             globals: {
-                window: "readonly",
-                document: "readonly",
-                console: "readonly",
+                ...globals.browser,
             },
         },
 
