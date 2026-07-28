88c63483d805e528ab58e955549a25a2205595fa
Tristan <tristanacosta03@gmail.com>
2026-07-28 13:20:06 +0800

    Complete styling on the header

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import "./styles/main.css";
 import Sunny from "./assets/icons/sunny.svg?raw";
 import Wind from "./assets/icons/wind.svg?raw";
 import Humidity from "./assets/icons/humidity.svg?raw";
+import Location from "./assets/icons/location.svg?raw";
 
 const bg = document.getElementById("bg");
 const wind = document.getElementById("wind");
 const humidity = document.getElementById("humidity");
+const location = document.getElementById("location");
 
 const icons = {
     bg: Sunny,
     wind: Wind,
     humidity: Humidity,
+    location: Location,
 };
 
 bg.innerHTML = icons.bg;
 wind.innerHTML = icons.wind;
 humidity.innerHTML = icons.humidity;
+location.innerHTML = icons.location;
--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,6 @@ export default {
                 test: /\.(png|jpg|jpeg|gif)$/i,
                 type: "asset/resource",
             },
-            ,
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: "asset/resource",
